a24b8ef4bb2e73f6f11b0963c285d250ceda9887
David <david_andika17123@hotmail.com>
2019-09-24 17:22:50 +1000

    Initial commit

---
+++ src/index.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const getMountPoint = (id = 'root') => {
+  let rootEl = document.getElementById(id)
+  if (!rootEl) {
+    rootEl = document.createElement('div')
+    rootEl.id = id
+    document.getElementsByTagName('body')[0].appendChild(rootEl)
+  }
+  return rootEl
+}
+
+ReactDOM.render(
+  (
+    <div>
+      Hello world
+    </div>
+  ),
+  getMountPoint('root'),
+)
---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const webpackConfig = (env = {}) => {
+  const isProduction = !!env.production
+
+  const config = {
+    mode: isProduction ? 'production' : 'development',
+    entry: './src/index.jsx',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: '[name].[hash].js',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          exclude: /node_modules/,
+          use: ['babel-loader'],
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.js', '.jsx'],
+    },
+    plugins: [
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({
+        title: 'Login',
+      }),
+    ],
+  }
+
+  if (!isProduction) {
+    config.devtool = 'inline-source-map'
+    config.devServer = {
+      contentBase: path.join(__dirname, 'dist'),
+      host: '0.0.0.0',
+      hot: true,
+    }
+  }
+
+  return config
+}
+
+module.exports = webpackConfig
